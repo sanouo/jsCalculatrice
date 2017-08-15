a982e9974c06f8caae5f070c93d481e348f70c03
sanouo <sanouo591@gmail.com>
2017-08-15 17:03:09 +0200

    create script for the calculator

---
+++ js/main.js
@@ -0,0 +1,117 @@
+function zero (){
+var zero = document.getElementById("zero").value;
+var input = document.getElementById("affiche").value;
+document.getElementById("affiche").value = input + zero;
+}
+
+function un (){
+var un = document.getElementById("un").value;
+var input = document.getElementById("affiche").value;
+document.getElementById("affiche").value = input + un;
+}
+
+function deux (){
+var deux = document.getElementById("deux").value;
+var input = document.getElementById("affiche").value;
+document.getElementById("affiche").value = input + deux;
+}
+
+function trois (){
+var trois = document.getElementById("trois").value;
+var input = document.getElementById("affiche").value;
+document.getElementById("affiche").value = input + trois;
+}
+
+function quatre (){
+var quatre = document.getElementById("quatre").value;
+var input = document.getElementById("affiche").value;
+document.getElementById("affiche").value = input + quatre;
+}
+
+function cinq (){
+var cinq = document.getElementById("cinq").value;
+var input = document.getElementById("affiche").value;
+document.getElementById("affiche").value = input + cinq;
+}
+
+function six (){
+var six = document.getElementById("six").value;
+var input = document.getElementById("affiche").value;
+document.getElementById("affiche").value = input + six;
+}
+
+function sept (){
+var sept = document.getElementById("sept").value;
+var input = document.getElementById("affiche").value;
+document.getElementById("affiche").value = input + sept;
+}
+
+function huit (){
+var huit = document.getElementById("huit").value;
+var input = document.getElementById("affiche").value;
+document.getElementById("affiche").value = input + huit;
+}
+
+function neuf (){
+var neuf = document.getElementById("neuf").value;
+var input = document.getElementById("affiche").value;
+document.getElementById("affiche").value = input + neuf;
+}
+
+function point (){
+var point = document.getElementById("point").value;
+var input = document.getElementById("affiche").value;
+document.getElementById("affiche").value = input + point;
+}
+
+function parentheseg (){
+var parentheseg = document.getElementById("parentheseg").value;
+var input = document.getElementById("affiche").value;
+document.getElementById("affiche").value = input + parentheseg;
+}
+
+function parenthesed (){
+var parenthesed = document.getElementById("parenthesed").value;
+var input = document.getElementById("affiche").value;
+document.getElementById("affiche").value = input + parenthesed;
+}
+
+function egal (){
+var egal = document.getElementById("affiche").value;
+var total = eval(egal);
+document.getElementById("affiche").value = total;
+}
+
+function plus(){
+var plus = document.getElementById("plus").value;
+var input = document.getElementById("affiche").value;
+document.getElementById("affiche").value = input + plus;
+}
+
+function moins(){
+var moins = document.getElementById("moins").value;
+var input = document.getElementById("affiche").value;
+document.getElementById("affiche").value = input + moins;
+}
+
+function multiplier(){
+var multiplier = document.getElementById("multiplier").value;
+var input = document.getElementById("affiche").value;
+document.getElementById("affiche").value = input + multiplier;
+}
+
+function diviser(){
+var diviser = document.getElementById("diviser").value;
+var input = document.getElementById("affiche").value;
+document.getElementById("affiche").value = input + diviser;
+}
+
+function pourcent(){
+var pourcent = document.getElementById("pourcent").value;
+var input = document.getElementById("affiche").value;
+document.getElementById("affiche").value = input + pourcent;
+}
+
+function supprime(){
+document.getElementById("affiche").value = "";
+}
